a972c3745aa777c7f5757d12319b82b9041275b8
onatm <onatmercan@gmail.com>
2021-02-16 23:06:02 +0000

    Get next run date using chrono

--- src/cron_table.rs
+++ src/cron_table.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Datelike, TimeZone, Timelike, Utc};
 use linked_hash_set::LinkedHashSet;
 use std::str::FromStr;
 
@@ -26,6 +27,96 @@ impl FromStr for CronTab {
 }
 
 impl CronTab {
+    pub fn next(&self, after: &DateTime<Utc>) -> Option<DateTime<Utc>> {
+        let base_year = after.year();
+        let base_month = after.month();
+        let base_day = after.day();
+        let base_hour = after.hour();
+        let base_minute = after.minute();
+
+        let mut year = base_year;
+        let mut month = base_month;
+        let mut day = base_day;
+        let mut hour = base_hour;
+        let mut minute = base_minute;
+
+        let minute_range: LinkedHashSet<u32> = (minute..=59).collect();
+        let minutes: LinkedHashSet<u32> = self.minute.clone().into_iter().collect();
+        let minutes = minute_range.intersection(&minutes).collect::<Vec<&u32>>();
+
+        if let Some(&&first_minute) = minutes.first() {
+            minute = first_minute;
+        } else {
+            minute = self.minute[0].clone();
+            hour += 1;
+        }
+
+        let hour_range: LinkedHashSet<u32> = (hour..=23).collect();
+        let hours: LinkedHashSet<u32> = self.hour.clone().into_iter().collect();
+        let hours = hour_range.intersection(&hours).collect::<Vec<&u32>>();
+
+        if let Some(&&first_hour) = hours.first() {
+            if first_hour > base_hour {
+                minute = self.minute[0].clone();
+            } else {
+                hour = first_hour;
+            }
+        } else {
+            minute = self.minute[0].clone();
+            hour = self.hour[0].clone();
+            day += 1;
+        }
+
+        let day_range: LinkedHashSet<u32> = (day..=31).collect();
+        let days: LinkedHashSet<u32> = self.day_of_month.clone().into_iter().collect();
+        let days = day_range.intersection(&days).collect::<Vec<&u32>>();
+
+        if let Some(&&first_day) = days.first() {
+            println!("{:?}", first_day);
+            if first_day > base_day {
+                minute = self.minute[0].clone();
+                hour = self.hour[0].clone();
+            } else {
+                day = first_day;
+            }
+        } else {
+            minute = self.minute[0].clone();
+            hour = self.hour[0].clone();
+            day = self.day[0].clone();
+            month += 1;
+        }
+
+        let month_range: LinkedHashSet<u32> = (month..=12).collect();
+        let months: LinkedHashSet<u32> = self.month.clone().into_iter().collect();
+        let months = month_range.intersection(&months).collect::<Vec<&u32>>();
+
+        if let Some(&&first_month) = months.first() {
+            if first_month > base_month {
+                minute = self.minute[0].clone();
+                hour = self.hour[0].clone();
+                day = self.day[0].clone();
+            } else {
+                month = first_month;
+            }
+        } else {
+            minute = self.minute[0].clone();
+            hour = self.hour[0].clone();
+            day = self.day[0].clone();
+            month = self.month[0].clone();
+            year += 1;
+        }
+
+        let next_time = Utc.ymd(year, month, day).and_hms(hour, minute, 0);
+
+        let day_of_week = next_time.weekday().number_from_sunday() - 1;
+
+        if self.day.contains(&day_of_week) {
+            Some(next_time)
+        } else {
+            None
+        }
+    }
+
     pub fn from_cron_expression_list(
         minute_list: Vec<CronExpression>,
         hour_list: Vec<CronExpression>,
