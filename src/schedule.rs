809997aa2f18d1f534fb01e207a16e3a810ff49e
onatm <onatmercan@gmail.com>
2021-02-16 00:08:13 +0000

    Implement parser for minutes only

--- src/schedule.rs
+++ src/schedule.rs
@@ -1,3 +1,4 @@
+use linked_hash_set::LinkedHashSet;
 use std::{error, fmt::Display, str::FromStr};
 
 use crate::parser::Parser;
@@ -11,13 +12,67 @@ impl Display for Error {
 }
 impl error::Error for Error {}
 
+#[derive(Debug, PartialEq)]
+pub enum CronBaseExpression {
+    All,
+    Exact(u32),
+    Range(u32, u32),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum CronExpression {
+    Simple(CronBaseExpression),
+    Period(CronBaseExpression, u32),
+}
+
 #[derive(Debug)]
-pub struct Schedule;
+pub struct Schedule {
+    pub minutes: Vec<u32>,
+}
 
 impl FromStr for Schedule {
     type Err = Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Parser::parse(s))
+    fn from_str(expression: &str) -> Result<Self, Self::Err> {
+        Parser::parse(expression)
+    }
+}
+
+impl Schedule {
+    pub fn from_cron_expression_list(list: Vec<CronExpression>) -> Schedule {
+        let mut set = LinkedHashSet::<u32>::new();
+        for expression in list {
+            println!("{:?}", expression);
+            let inner = Self::from_cron_expression(expression);
+            set.extend(inner);
+        }
+        let mut items: Vec<u32> = set.into_iter().collect();
+        items.sort();
+        Schedule { minutes: items }
+    }
+
+    fn from_cron_expression(expression: CronExpression) -> LinkedHashSet<u32> {
+        match expression {
+            CronExpression::Simple(expression) => Self::from_cron_base_expression(expression),
+            CronExpression::Period(start, step) => {
+                let set = match start {
+                    CronBaseExpression::Exact(start) => (start..=23).collect(),
+                    expression => Self::from_cron_base_expression(expression),
+                };
+                set.into_iter().step_by(step as usize).collect()
+            }
+        }
+    }
+
+    fn from_cron_base_expression(expression: CronBaseExpression) -> LinkedHashSet<u32> {
+        match expression {
+            CronBaseExpression::All => (0..=23).collect(),
+            CronBaseExpression::Exact(number) => {
+                let mut set = LinkedHashSet::new();
+                set.insert(number);
+                set
+            }
+            CronBaseExpression::Range(start, end) => (start..=end).collect(),
+        }
     }
 }
